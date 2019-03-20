c320b03b73f296a58e79d02d685ea8d46655cdaa
mike connor <mike@rocketnumber9.org>
2019-03-20 17:34:04 -0500

    BAP.changePosition

--- docs/js/ba.js
+++ docs/js/ba.js
@@ -3107,9 +3107,33 @@ var BAP =
       };
     } 
 
-    BAP.attach = function(){
-console.log('attach');
+    BAP.changePosition = function(...args){
+      
+      if (isElement(arg[0])){
+        console.log('you sent in a dom element');
+      } else if (typeof arg[0] === 'string') {
+        console.log('you sent in a string');
+      }
+
+    
+    };
+
+    function isElement(obj) {
+      try {
+        //Using W3 DOM2 (works for FF, Opera and Chrome)
+        return obj instanceof HTMLElement;
+      }
+      catch(e){
+        //Browsers not supporting W3 DOM2 don't have HTMLElement and
+        //an exception is thrown and we end up here. Testing some
+        //properties that all elements have (works on IE7)
+        return (typeof obj==="object") &&
+          (obj.nodeType===1) && (typeof obj.style === "object") &&
+          (typeof obj.ownerDocument ==="object");
+      }
     }
+
+
     
     try {
       addEvent(window, "message", handleMessage);
@@ -3166,7 +3190,7 @@ console.log('attach');
     if (window._bao) {
       start(window._bao);
     }
-    API.attach = BAP.attach;
+    API.changePosition = BAP.changePosition;
     API.options = BAP.options;
     API.flashPostMessage = flashPostMessage;
     API.createL2 = createL2;
