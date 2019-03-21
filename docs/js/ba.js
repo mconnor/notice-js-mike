a183be5fd2b1a4e32ff0910c06c9d1c5b53eeedd
mike connor <mike@rocketnumber9.org>
2019-03-21 14:30:54 -0500

    fix innerHTML

--- docs/js/ba.js
+++ docs/js/ba.js
@@ -3134,9 +3134,8 @@ var BAP =
         
         BAP.options[pageId].position = position;
         
-        document.querySelector()
-
-        document.querySelector('#trigger-box-'+pageId).innerHTML = '<img id="trigger-box-image-' + pageId + '"' + 'src="http://dev.betrad.com/icon/box_77_'+ position + '.png">';
+    
+        document.querySelector('#trigger-'+pageId).innerHTML = '<img src="http://dev.betrad.com/icon/box_77_' + position + '.png">';
         //noticeCreate(pageId);
         //noticePosition(pageId);
 
