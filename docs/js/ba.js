a899548bc6fa56a03625b16e7ef882a8cc36b58e
mike connor <mike@rocketnumber9.org>
2019-04-04 12:21:55 -0500

    _iconDomElement

--- docs/js/ba.js
+++ docs/js/ba.js
@@ -2296,7 +2296,8 @@ var BAP =
       try {
         var ad_css_position = getComputedStyle(BAP.options[pageId].ad).position;
         if (BAP.options[pageId].dm === 3 && (ad_css_position === 'relative' || ad_css_position === 'absolute')) {
-          var _iconDomElement = document.createElement(icon);
+          var _iconDomElement = document.createElement('span');
+          _iconDomElement.innerHTML += icon;
           BAP.options[pageId].ad.appendChild(_iconDomElement);
          // BAP.options[pageId].ad.innerHTML += icon;
           setTimeout(positionDM3(pageId),1000);
