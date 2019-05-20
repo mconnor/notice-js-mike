c0f0c8cd169818fbd296daffdeb962c11d7e32ce
mike connor <mike@rocketnumber9.org>
2019-05-20 17:31:30 -0500

    padding 8px

--- docs/js/ba.js
+++ docs/js/ba.js
@@ -408,7 +408,7 @@ var BAP =
         color: rgb(0, 0, 0); \
         margin: 0 0 0 0; \
         font-size: .9em; \
-        padding: 4px 5px 6px 10px; \
+        padding: 4px 5px 6px 8px; \
       } \
       .main-copy-new-l2 { \
         color: rgb(138, 135, 135); \
