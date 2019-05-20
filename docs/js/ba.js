8839fb7e0b079dd19ea6e34af6a01782ede59585
mike connor <mike@rocketnumber9.org>
2019-05-20 14:20:37 -0500

    img padding/margin

--- docs/js/ba.js
+++ docs/js/ba.js
@@ -303,9 +303,7 @@ var BAP =
       ".bap-img-container { \
         width: 124px; \
         height: 45px; \
-      } \
-      .padding-left-10 { \
-        padding-left: 10px; \
+        margin-top: 4px 0 0 10px; \
       } \
       .center-horiz { \
         margin: 0 auto 0 auto; \
@@ -2997,7 +2995,7 @@ var BAP =
         reg = 6;
       }
 
-      var logoCssClasses = BAP.options[pageId].new_l2 ? "bap-img-container center-horiz" : "bap-img-container padding-left-10";
+      var logoCssClasses = BAP.options[pageId].new_l2 ? "bap-img-container center-horiz" : "bap-img-container";
 
       BAP_ADVERTISER_LOGO = '<div class="' + logoCssClasses +'">' +
           (BAP.options[pageId].advLogo
