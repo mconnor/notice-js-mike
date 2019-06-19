7153fa3ae09db21b30719e0be8d908cd70d56775
mike connor <mike@rocketnumber9.org>
2019-06-19 12:19:33 -0500

    work around getElementById
    new way to get params off of tag without using a tag id

--- docs/evidon-vpaid.js
+++ docs/evidon-vpaid.js
@@ -1,29 +1,39 @@
 (function() {
-  if (document.getElementById("evidon-vpaid")) {
-    var durlyParmaString = document
-      .getElementById("evidon-vpaid")
-      .src.split("?")[1];
-    var adIFrame = window.frameElement;
-    var durlyScript = document.createElement("SCRIPT");
-    durlyScript.setAttribute("type", "text/javascript");
-    durlyScript.setAttribute("data-name", "durly");
-    if (adIFrame.clientWidth) {
-      durlyParmaString = durlyParmaString.concat(
-        ";ad_w=" + adIFrame.clientWidth
-      );
+    var _scripts = document.getElementsByTagName("SCRIPT");
+    for (var i = 0; i < _scripts.length; i++) {
+        var _script = _scripts[i],
+            _src;
+        // skip if no source exists
+        if (!_script.src) {
+            continue;
+        }
+        _src = _script.src;
+
+        if (_src.match("evidon-vpaid.js")) {
+            var durlyParmaString = document
+                .getElementById("evidon-vpaid")
+                .src.split("?")[1];
+            var adIFrame = window.frameElement;
+            var durlyScript = document.createElement("SCRIPT");
+            durlyScript.setAttribute("type", "text/javascript");
+            durlyScript.setAttribute("data-name", "durly");
+            if (adIFrame.clientWidth) {
+                durlyParmaString = durlyParmaString.concat(
+                    ";ad_w=" + adIFrame.clientWidth
+                );
+            }
+            if (adIFrame.clientHeight) {
+                durlyParmaString = durlyParmaString.concat(
+                    ";ad_h=" + adIFrame.clientHeight
+                );
+            }
+            durlyParmaString = durlyParmaString.concat(";vpaid=true");
+            durlyScript.setAttribute(
+                "src",
+                "./surly/durly.js?" + durlyParmaString
+            );
+            adIFrame.contentWindow.document.body.appendChild(durlyScript);
+
+        }
     }
-    if (adIFrame.clientHeight) {
-      durlyParmaString = durlyParmaString.concat(
-        ";ad_h=" + adIFrame.clientHeight
-      );
-    }
-    durlyParmaString = durlyParmaString.concat(";vpaid=true");
-    durlyScript.setAttribute(
-      "src",
-      "./surly/durly.js?" + durlyParmaString
-    );
-    adIFrame.contentWindow.document.body.appendChild(durlyScript);
-  } else {
-    console.warn("No script element found with ‘evidon-vpaid’ identifier");
-  }
 })();
