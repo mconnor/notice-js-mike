39285e24ec4e3341c67b3378fbde17a8b93c05e6
mike connor <mike@rocketnumber9.org>
2019-08-07 18:32:57 -0500

    fix isNonTimerDm

--- docs/js/ba.js
+++ docs/js/ba.js
@@ -1958,7 +1958,7 @@ var BAP =
     }
 
     function isNonTimerDm(_dm){
-      if (_dm === 9 || _dm === 5){
+      if ((_dm ===3 || _dm === 9 || _dm === 5) && (BAP.options[18984].ad.style.position == 'relative' || BAP.options[18984].ad.style.position == 'absolute')){
         return true;
       } else {
         return false;
